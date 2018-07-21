8205bd68a21f7dbf094abd93861bf4fab14c6d58
Stefan Heimberg <kontakt@stefanheimberg.ch>
2018-07-21 14:05:26 +0200

    wildfly hinzugefügt

---
+++ run-wildfly.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -e
+
+__dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+cd ${__dir}
+
+if [ ! -f "deployments/ee7-ejb-cdi-scopemix-ear.ear" ]; then
+    echo "ERROR: run ./build.sh first"
+    exit 1
+fi
+
+if [ -f "logs/example.log" ]; then
+    echo "" > logs/example.log
+fi
+
+docker run -i -t --rm \
+    -p 8080:8080 \
+    -p 9990:9990 \
+    -v ${__dir}/deployments:/opt/jboss/wildfly/standalone/deployments \
+    -v ${__dir}/logs:/opt/jboss/logs \
+    jboss/wildfly:13.0.0.Final \
+    /opt/jboss/wildfly/bin/standalone.sh \
+    -bmanagement 0.0.0.0
